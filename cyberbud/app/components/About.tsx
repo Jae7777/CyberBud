e9a417328d0786b2838aa662e8bb5bf1cc99f7cf
lForger <navajaydan@gmail.com>
2023-11-04 15:39:12 -0400

    Added text gradient

---
+++ cyberbud/app/components/About.tsx
@@ -0,0 +1,14 @@
+export default function About(){
+    return(
+        <div className="text-center" id="about"> 
+            <h1 className="text-6xl">
+                What is Cyberbud?
+            </h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in justo vel justo facilisis bibendum. Duis at urna vel mi euismod fringilla.</p>
+            <p>Vestibulum consectetur, nulla sed tincidunt volutpat, ligula est posuere turpis, eget varius quam felis eu purus. Nullam id justo nec urna ullamcorper facilisis.</p>
+            <p>Proin in mauris quam. Vivamus ac quam nec dolor ultrices congue. Sed non arcu id sapien tincidunt elementum vel nec mi.</p>
+            <p>Curabitur tristique justo vel sagittis vehicula. Nunc tincidunt tortor vitae nisi euismod, a bibendum metus aliquam. Sed ac eros vel arcu congue malesuada.</p>
+            <p>Pellentesque et libero eget odio euismod bibendum. Sed suscipit massa in ligula auctor, ut condimentum metus egestas. Nullam quis efficitur sapien.</p>
+        </div>
+    )
+}
--- cyberbud/app/page.tsx
+++ cyberbud/app/page.tsx
@@ -1,11 +1,64 @@
 import Image from 'next/image'
-
+import About from './components/About'
+import Link from 'next/link'
 export default function Home() {
   return (
-    <>
-      <div>
-
+    <main className="flex flex-col items-center justify-between p-24">
+      <div className="relative bg-gradient-to-r from-slate-400 to-white inline-block text-transparent bg-clip-text">
+        <h1 className="text-6xl">
+          Cyberbud
+        </h1>
       </div>
-    </>
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-3 lg:text-left">
+        <Link 
+         href="/#"
+         className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            About{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Find about more about our website
+          </p>
+         </Link>
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Templates{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Explore the Next.js 13 playground.
+          </p>
+        </a>
+
+        <a
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Deploy{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          </p>
+        </a>
+      </div>
+      <About></About>
+    </main>
   )
 }
