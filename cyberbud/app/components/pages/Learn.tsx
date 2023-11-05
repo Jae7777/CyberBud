aa1d67af2ae85cbba58976521338961091d3c4ac
lForger <navajaydan@gmail.com>
2023-11-04 22:49:50 -0400

    Added more to vuln list

--- cyberbud/app/components/pages/Learn.tsx
+++ cyberbud/app/components/pages/Learn.tsx
@@ -1,8 +1,9 @@
 import VulnerabilityList from "../VulnerabilityList"
 export default function Learn(){
     return(
-        
-        <h1 className="text-6xl">Common Vulnerabilites</h1>
-        <VulnerabilityList></VulnerabilityList>
+        <>
+            <h1 className="text-6xl">Common Vulnerabilites</h1>
+            <VulnerabilityList></VulnerabilityList>
+        </>
     )
 }
